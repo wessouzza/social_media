a5e8b99199db4463f90387ba488235519325e11e
wessouzza <wes-souza@hotmail.com>
2024-04-29 16:51:08 -0300

    Feat: Post entity

---
+++ src/main/java/com/example/social_media/entities/Post.java
@@ -0,0 +1,103 @@
+package com.example.social_media.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_posts")
+public class Post implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "This field can't be emtpy.")
+    @Size(max = 200, message = "The limit is 200 characters.")
+    private String body;
+
+    private LocalDateTime publicationDate;
+    
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    @JoinColumn(name = "post_id")
+    private List<Comments> comments = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "post_id")
+    private List<Likes> likes = new ArrayList<>();
+
+    public Post(){}
+
+    public Post(Long id, String body, LocalDateTime publicationDate) {
+        this.id = id;
+        this.body = body;
+        this.publicationDate = publicationDate;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+    public LocalDateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDateTime publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Comments> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comments> comments) {
+        this.comments = comments;
+    }
+
+    public List<Likes> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Likes> likes) {
+        this.likes = likes;
+    }
+
+
+}
